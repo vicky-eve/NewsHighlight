a7af3f17c2b81c7778b0c2537b0506259273f533
vicky <victoria.awuor@student.moringaschool.com>
2022-02-26 09:30:51 +0300

    created two classes for news source and articles

---
+++ app/models/news.py
@@ -0,0 +1,25 @@
+class News:
+        """
+        Articles class to define articles object
+        """
+def __init__(self, id, name, author, title, description, url):
+        self.id =id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+
+class Articles:
+        """
+        Articles class to define articles object
+        """
+def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt, content):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
