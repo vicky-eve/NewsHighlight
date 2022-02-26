e3ad83b1d1464b532c52be42296c3b4d99a75fa6
vicky <victoria.awuor@student.moringaschool.com>
2022-02-26 18:34:52 +0300

    created a function that processes the results and create articles objects from the elements we need

--- app/request.py
+++ app/request.py
@@ -49,3 +49,47 @@ def process_results(source_list):
             source_results.append(source_object)
 
     return source_results
+
+def get_articles(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['results']:
+            articles_results_list = get_articles_response['results']
+            articles_results = process_results(articles_results_list)
+
+
+    return articles_results
+
+def process_results(articles_list):
+    '''
+    Function  that processes the articles result and transform them to a list of Objects
+
+    Args:
+        articles_list: A list of dictionaries that contain articles details
+
+    Returns :
+        articles_results: A list of articles objects
+    '''
+    articles_results = []
+    for articles_item in articles_list:
+        id = articles_item.get('id')
+        name = articles_item.get('original_title')
+        author = articles_item.get('author')
+        title = articles_item.get('')
+        description= articles_item.get('description')
+        url = articles_item.get('url')
+
+        if id:
+            articles_object = Articles (id, name, author, title, description,url)
+            articles_results.append(articles_object)
+
+    return articles_results
