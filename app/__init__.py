c2d0d00a52770bd3822c35c942c7eafcb0f541a7
vicky <victoria.awuor@student.moringaschool.com>
2022-02-27 14:10:25 +0300

    created an init file in main  folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,25 @@
 from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap   #initialise boostrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
 
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+    # Initializing application
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+   # Initializing flask extensions
+    bootstrap.init_app(app)
 
-from app import views
-from app import error
+
+    from app import views
+    from app import error
+
+    return app
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from app import app
-from .request import get_source , get_articles
+from .request import get_source , get_articles, search_articles
 from flask import render_template           #import render template since i'll need to render a template
 
 @app.route('/')
@@ -22,3 +22,14 @@ def articles(articles_id):
     print(articles)
 
     return render_template('art.html', message2 = articles)
+
+@app.route('/search/<artcles_name>')
+def search(articles_name):      #dynamic variable
+    '''
+    View function to display the search results
+    '''
+    articles_name_list = articles_name.split(" ")
+    articles_name_format = "+".join(articles_name_list)
+    searched_articles = search_articles(articles_name_format)
+    title = f'search results for {articles_name}'
+    return render_template('search.html',movies = searched_articles)
