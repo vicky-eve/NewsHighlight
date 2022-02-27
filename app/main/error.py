c71906d168e9349c7ed636bf5b9965b920b94982
vicky <victoria.awuor@student.moringaschool.com>
2022-02-27 14:29:30 +0300

    added an application wide error handler

--- app/main/error.py
+++ app/main/error.py
@@ -1,7 +1,15 @@
 from flask import render_template
 from app import app
+from . import main
 
 @app.errorhandler(404)    #decorator to pass in the error we receive
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404
+
+@main.app_errorhandler(404)    #to enable application-iwide use
 def four_Ow_four(error):
     '''
     Function to render the 404 error page
